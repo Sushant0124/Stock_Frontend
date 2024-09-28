5131f9d3baffde8fe77bfcd6c08e7a50dfa5f15b
SUSHANT BANSAL <142045977+Sushant0124@users.noreply.github.com>
2024-09-28 23:52:30 +0530

    Update Hero.jsx

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function Hero() {
     return ( 
@@ -8,7 +9,7 @@ function Hero() {
        <img src='/images/homeHero.png' alt='HeroImage' className="hero-image" />
        <h1 className="hero-title">Invest in everything</h1>
        <h5 className="hero-description" >Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</h5>
-       <button type="button" className="btn btn-primary hero-button">Sign Up Now</button>
+      <Link to="/signup"> <button type="button" className="btn btn-primary hero-button">Sign Up Now</button></Link>
         </div>
         </>
      );
