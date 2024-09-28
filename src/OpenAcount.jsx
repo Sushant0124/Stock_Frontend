d5c3f9beb1d2c3108bcd790fed8d44786a284fd4
SUSHANT BANSAL <142045977+Sushant0124@users.noreply.github.com>
2024-09-29 00:01:20 +0530

    Update OpenAcount.jsx

--- src/OpenAcount.jsx
+++ src/OpenAcount.jsx
@@ -7,7 +7,7 @@ function OpenAccount() {
         <div className='openAccount'>
         <h2 className="oc-title">Open a StockFusion account</h2>
        <h5 className="oc-description" >Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</h5>
-      <Link to="/signup> <button type="button" className="btn btn-primary oc-button ">Sign Up Now</button></Link>
+      <Link to="/signup"> <button type="button" className="btn btn-primary oc-button ">Sign Up Now</button></Link>
             
         </div>
         </>
